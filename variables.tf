9f07ef054509083cbaa18ec4b4b904270e441c26
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-10-17 00:38:28 -0500

    variables por defecto

--- variables.tf
+++ variables.tf
@@ -15,23 +15,25 @@ variable "service" {
 
 variable "cidr_block_vpc" {
   type    = string
-  # default = "10.0.0.0/16"
+  default = null
 }
 
 variable "cidr_block_pri" {
   type    = list(string)
-  # default = ["10.0.0.0/18", "10.0.64.0/18"]
+  default = []
 }
 
 variable "cidr_block_pub" {
   type    = list(string)
-  # default = ["10.0.128.0/18", "10.0.192.0/18"]
+  default = []
 }
 
 variable "enable_dns_support" {
-  type = bool
+  type    = bool
+  default = true
 }
 
 variable "enable_dns_hostnames" {
-  type = bool
+  type    = bool
+  default = true
 }
